748cd4b1ff6e9faf02029627c2074add20c99e80
Joy Panchal <joypanchal971@gmail.com>
2019-03-05 20:31:20 -0500

    Further implement

--- app/src/main/java/com/jk/kotlinassignment_1/GeneralMoters.kt
+++ app/src/main/java/com/jk/kotlinassignment_1/GeneralMoters.kt
@@ -1,3 +1,30 @@
 package com.jk.kotlinassignment_1
 
 class GeneralMoters (var carName : String , var releaseYear : Int,var horsePower : Int,var trailer : Boolean,var rearCamera : Boolean,var price : Double)
+{
+
+    internal class SortbyCarName : Comparator<GeneralMoters> {
+        // Used for sorting in ascending order of
+        // car name
+        override fun compare(a: GeneralMoters, b: GeneralMoters): Int {
+            return a.carName.compareTo(b.carName)
+        }
+    }
+
+    internal class SortbyCarPrice : Comparator<GeneralMoters> {
+        // Used for sorting in ascending order of
+        // price
+        override fun compare(a: GeneralMoters, b: GeneralMoters): Int {
+            return a.price.compareTo(b.price)
+        }
+    }
+
+    internal class SortbyCarYear : Comparator<GeneralMoters> {
+        // Used for sorting in ascending order of
+        // price
+        override fun compare(a: GeneralMoters, b: GeneralMoters): Int {
+            return a.releaseYear.compareTo(b.releaseYear)
+        }
+    }
+
+}
--- app/src/main/java/com/jk/kotlinassignment_1/MainActivity.kt
+++ app/src/main/java/com/jk/kotlinassignment_1/MainActivity.kt
@@ -3,38 +3,94 @@ package com.jk.kotlinassignment_1
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity()
-{
-    var carList = ArrayList<GeneralMoters>()
+class MainActivity : AppCompatActivity() {
+    //var carList = ArrayList<GeneralMoters>()
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        addData()
+        /*addData()
 
         sortByPrice()
         sortByYear()
-        sortByName()
+        sortByName()*/
 
-        
+        var carList = ArrayList<GeneralMoters>()
+
+        carList.add(
+            GeneralMoters(
+                "Chevrolet Cruze", 2001, 800, false, true,
+                19000.00
+            )
+        )
+        carList.add(
+            GeneralMoters(
+                "Chevrolet Malibu", 2010, 1200, false, true,
+                30000.00
+            )
+        )
+        carList.add(
+            GeneralMoters(
+                "GMC terrain", 2008, 1500, true, true,
+                22000.00
+            )
+        )
+        carList.add(
+            GeneralMoters(
+                "Cadillac XT6", 2019, 1300, false, true,
+                45000.00
+            )
+        )
+        carList.add(
+            GeneralMoters(
+                "Buick Encore", 2011, 600, false, false,
+                12000.00
+            )
+        )
+        carList.add(
+            GeneralMoters(
+                "Buick Regal", 1970, 1100, false, true,
+                28000.00
+            )
+        )
+
+        println("----- Sort BY Name ------")
+        carList.sortWith(GeneralMoters.SortbyCarName())
+        for (i in carList) {
+            println(i.carName + "        " + i.releaseYear + "        " + i.horsePower + "        " + i.rearCamera + "        " + i.price)
+        }
+
+        println("----- Sort BY Price ------")
+        carList.sortWith(GeneralMoters.SortbyCarPrice())
+        for (i in carList) {
+            println(i.carName + "        " + i.releaseYear + "        " + i.horsePower + "        " + i.rearCamera + "        " + i.price)
+        }
+
+        println("----- Sort BY Year ------")
+        carList.sortWith(GeneralMoters.SortbyCarYear())
+        for (i in carList) {
+            println(i.carName + "        " + i.releaseYear + "        " + i.horsePower + "        " + i.rearCamera + "        " + i.price)
+        }
+
+
     }
-
+}
+/*
     fun addData()
     {
 
-        carList.add(GeneralMoters("Chevrolet Cruze",2001,800,false,true,
+        carList.add(ModelData("Chevrolet Cruze",2001,800,false,true,
             19000.00))
-        carList.add(GeneralMoters("Chevrolet Malibu",2010,1200,false,true,
+        carList.add(ModelData("Chevrolet Malibu",2010,1200,false,true,
             30000.00))
-        carList.add(GeneralMoters("GMC terrain",2008,1500,true,true,
+        carList.add(ModelData("GMC terrain",2008,1500,true,true,
             22000.00))
-        carList.add(GeneralMoters("Cadillac XT6",2019,1300,false,true,
+        carList.add(ModelData("Cadillac XT6",2019,1300,false,true,
             45000.00))
-        carList.add(GeneralMoters("Buick Encore",2011,600,false,false,
+        carList.add(ModelData("Buick Encore",2011,600,false,false,
             12000.00))
-        carList.add(GeneralMoters("Buick Regal",1970,1100,false,true,
+        carList.add(ModelData("Buick Regal",1970,1100,false,true,
             28000.00))
     }
 
@@ -42,7 +98,7 @@ class MainActivity : AppCompatActivity()
     fun sortByPrice()
     {
         var sortByPriceList = carList.sortedWith(compareBy({it.price}))
-        println("1.-------------------Cars list order by Price")
+        println("1.-------------------Cars list order by Price-------------------")
         for (i in sortByPriceList)
             println(i.carName +"        "+ i.releaseYear +"        "+ i.horsePower +"        "+ i.rearCamera +"        "+ i.price)
     }
@@ -50,7 +106,7 @@ class MainActivity : AppCompatActivity()
     fun sortByYear()
     {
         var sortByYearList = carList.sortedWith(compareBy({it.releaseYear}))
-        println("2.-------------------Cars list order by Release Year")
+        println("2.-------------------Cars list order by Release Year-------------------")
         for (i in sortByYearList)
             println(i.carName +"        "+ i.releaseYear +"        "+ i.horsePower +"        "+ i.rearCamera +"        "+ i.price)
     }
@@ -58,9 +114,10 @@ class MainActivity : AppCompatActivity()
     fun sortByName()
     {
         var sortByNameList = carList.sortedWith(compareBy({it.carName}))
-        println("3.-------------------Cars list order by Name")
+        println("3.-------------------Cars list order by Name-------------------")
         for (i in sortByNameList)
             println(i.carName +"        "+ i.releaseYear +"        "+ i.horsePower +"        "+ i.rearCamera +"        "+ i.price)
 
     }
 }
+*/
